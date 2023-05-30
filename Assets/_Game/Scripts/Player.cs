6f69796d25dbc73f2e8b35e3dfd223dab2bc6dd3
Vũ Nguyễn Tiến Lộc <106739170+callmeLoc@users.noreply.github.com>
2023-05-30 21:00:17 +0700

    Game2D _ Almost finish

---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpForce = 350;
+
+    [SerializeField] private Kunai kunaiPrefab;
+    [SerializeField] private Transform throwPoint;
+    [SerializeField] private GameObject attackArea;
+
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    private bool isAttack = false;
+    //private bool isDeath = false;
+
+    private float horizontal;
+
+    private int coin = 0;
+
+    private Vector3 savePoint;
+
+    void Update()
+    {
+        if (IsDead)
+        {
+            return;
+        }
+
+        isGrounded = CheckGrounded();
+
+        // nằm trong đoạn từ -1 -> 0 -> 1 (=-1 nếu bấm sang trái, =1 nếu bấm sang phải, =0 nếu k bấm)
+        horizontal = Input.GetAxisRaw("Horizontal"); // câu lệnh lấy điều khiển từ bàn phím
+        //verticle = Input.GetAxisRaw("Vertical"); // với chiều dọc
+
+        if (isAttack)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        if (isGrounded)
+        {
+            if (isJumping)
+            {
+                return;
+            }
+
+            //jump
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                Jump();
+            }
+
+            //change anim run
+            if (Mathf.Abs(horizontal) > 0.1f)
+            {
+                ChangeAnim("run");
+            }
+
+            //attack
+            if (Input.GetKeyDown(KeyCode.Q) && isGrounded)
+            {
+                Attack();
+            }
+
+            //throw
+            if (Input.GetKeyDown(KeyCode.E) && isGrounded)
+            {
+                Throw();
+            }
+        }
+
+        //check falling
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            ChangeAnim("fall");
+            isJumping = false;
+        }
+
+        //Moving
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); // horizontal * Time.deltaTime * speed là điều khiển theo trục x (điều khiển trái phải), rb.velocity.y theo trục y (nhảy sẽ rơi xuống)  
+            transform.rotation = Quaternion.Euler(new Vector3(0, horizontal > 0 ? 0 : 180, 0)); ;
+            //transform.localScale = new Vector3(horizontal, 1, 1);
+        } 
+        else if (isGrounded)
+        {
+            ChangeAnim("idle");
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+        isAttack = false;
+
+        transform.position = savePoint;
+        ChangeAnim("idle");
+        DeActiveAttack();
+
+        SavePoint();
+    }
+
+    public override void OnDespawn()
+    {
+        base.OnDespawn();
+        OnInit();
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+    }
+
+    private bool CheckGrounded()
+    {
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+
+        //if (hit.collider != null)
+        //{
+        //    return true;
+        //} else
+        //{
+        //    return false;
+        //}
+
+        return hit.collider != null;
+    }
+
+    private void Attack()
+    {
+        ChangeAnim("attack");
+        isAttack = true;
+        Invoke(nameof(ResetAttack), 0.5f);
+        ActiveAttack();
+        Invoke(nameof(DeActiveAttack), 0.5f);
+    }
+
+    private void Throw()
+    {
+        ChangeAnim("throw");
+        isAttack = true;
+        Invoke(nameof(ResetAttack), 0.5f);
+
+        Instantiate(kunaiPrefab, throwPoint.position, throwPoint.rotation);
+    }
+
+    private void ResetAttack()
+    {
+        ChangeAnim("ilde");
+        isAttack = false;
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+        ChangeAnim("jump");
+        rb.AddForce(jumpForce * Vector2.up);
+    }
+
+    internal void SavePoint()
+    {
+        savePoint = transform.position;
+    }
+
+    private void ActiveAttack()
+    {
+        attackArea.SetActive(true);
+    }
+
+    private void DeActiveAttack()
+    {
+        attackArea.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Coin")
+        {
+            coin++;
+            Destroy(collision.gameObject);
+        }
+        if (collision.tag == "DeathZone")
+        {
+            ChangeAnim("die");
+            Invoke(nameof(OnInit), 1f);
+        }
+    }
+}
